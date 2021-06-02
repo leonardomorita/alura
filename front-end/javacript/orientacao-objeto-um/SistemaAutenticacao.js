cb3c484599a33828dfa17d62185226e61089b9d1
Leonardo Morita <leonardomorita2@gmail.com>
2021-06-02 17:28:39 -0300

    Close #15

--- front-end/javacript/orientacao-objeto-um/SistemaAutenticacao.js
+++ front-end/javacript/orientacao-objeto-um/SistemaAutenticacao.js
@@ -1,5 +1,12 @@
 export class SistemaAutenticacao {
     static login(autenticavel, senha) {
+        if (!SistemaAutenticacao.ehAutenticavel(autenticavel)) return false;
+
         return autenticavel.autenticar(senha);
+    }
+
+    /* Verificar se a instância tem o método autenticar */
+    static ehAutenticavel(autenticavel) {
+        return "autenticar" in autenticavel && autenticavel.autenticar instanceof Function;
     }
 }
