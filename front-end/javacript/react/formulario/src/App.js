5fb48d1c8923b33a03b53826028301f1c17ea15e
Leonardo Morita <leonardomorita2@gmail.com>
2021-06-08 10:34:05 -0300

    Close #28

---
+++ front-end/javacript/react/formulario/src/App.js
@@ -0,0 +1,14 @@
+import { Fragment } from 'react';
+import FormularioCadastro from './components/FormularioCadastro';
+import './App.css';
+
+function App() {
+  return (
+    <Fragment>
+      <h1>Formulário de Cadastro</h1>
+      <FormularioCadastro />
+    </Fragment>
+  );
+}
+
+export default App;
